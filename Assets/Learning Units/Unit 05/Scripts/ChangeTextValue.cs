a5615c0810590c6995dfa8c8da04658e63fac18b
Caleb Bales <cbales2211@gmail.com>
2022-10-30 16:18:26 -0400

    Finished Level in HW
    Completed the Learning units in the homework

--- Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
+++ Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
@@ -9,16 +9,17 @@ public class ChangeTextValue : MonoBehaviour
     // the screen. You will need to add some stuff to this script to make it work...
 
     TextMeshProUGUI textMeshProUGUI;
-
+    public GameObject cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cube = GameObject.Find("Cube");
+        textMeshProUGUI.text = "Y: " + cube.transform.position.y.ToString();
     }
 }
